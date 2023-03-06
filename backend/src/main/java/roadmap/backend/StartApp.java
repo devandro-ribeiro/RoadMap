e98319a5226bc9736b04bc985496e1f6c74d42b6
evandro <evandroamado@gmail.com>
2023-03-06 20:12:34 -0300

    definindo entrada de dados via terminal para testes

--- backend/src/main/java/roadmap/backend/StartApp.java
+++ backend/src/main/java/roadmap/backend/StartApp.java
@@ -1,5 +1,7 @@
 package roadmap.backend;
 
+import java.util.Scanner;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,35 +12,60 @@ import roadmap.backend.repository.ClienteRepository;
 import roadmap.backend.repository.PedidoRepository;
 
 @Component
-public class StartApp implements CommandLineRunner{
-	
+public class StartApp implements CommandLineRunner {
+
 	@Autowired
 	private ClienteRepository repository;
 	@Autowired
 	private PedidoRepository repositoryPedido;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		//Insercoes setadas na mao para fins de testes
+		// Insercoes setadas na mao para fins de testes
 		Cliente cliente = new Cliente();
-		cliente.setCodigoCliente("JE312");
-		cliente.setNome("Maria");
-		cliente.setStatusCliente("A");
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("\nCadastro de Cliente\n");
+
+		System.out.println("Nome:");
+		cliente.setNome(scan.next());
+
+		System.out.println("Código do Cliente:");
+		cliente.setCodigoCliente(scan.next());
+
+		System.out.println("Status do Cliente:");
+		cliente.setStatusCliente(scan.next());
+
 		repository.save(cliente);
-		
-		for (Cliente string : repository.findAll()) {
-			System.out.println(string.getCodigoCliente()+" "+string.getNome()+" "+string.getStatusCliente());
-		}
-		
+
 		Pedido pedido = new Pedido();
-		pedido.setCodigoPedido("#1234");
-		pedido.setValorPedido(1587.0);
-		pedido.setPagamento(true);
+		System.out.println("\nCadastro de Pedido\n");
+		System.out.println("Código do pedido:");
+		pedido.setCodigoPedido(scan.next());
+
+		System.out.println("Valor do Pedido:");
+		pedido.setValorPedido(scan.nextDouble());
+
+		System.out.println("Status do Pagamento:");
+		String pagamento = scan.next();
+		if (pagamento.equals("ok"))
+			pedido.setPagamento(true);
+		else if (pagamento.equals("pendente"))
+			pedido.setPagamento(false);
+		else
+			System.out.println("Valor Inválido!");
+
+		pedido.setCliente(cliente);
+
 		repositoryPedido.save(pedido);
-		
-		for (Pedido string : repositoryPedido.findAll()) {
-			System.out.println(string.getCodigoPedido()+" "+string.getValorPedido()+" "+string.isPagamento());
+
+		for (Cliente string : repository.findAll()) {
+			System.out.println(string.getCodigoCliente() + "\n" + string.getNome() + "\n" + string.getStatusCliente());
 		}
+		for (Pedido string : repositoryPedido.findAll()) {
+			System.out.println(string.getCodigoPedido() + "\n" + string.getValorPedido() + "\n" + string.isPagamento());
+		}
+		
 	}
 
 }
